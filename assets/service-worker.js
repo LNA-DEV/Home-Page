e09ab7185d89da918d398269724b6d7bcc83df6c
Lukas Nagel <lukas@lna-dev.net>
2024-03-14 19:21:17 +0100

    ⚗️ All files in serviceWorker

--- assets/service-worker.js
+++ assets/service-worker.js
@@ -1,52 +1,76 @@
-// Change to a different app prefix name
 const APP_PREFIX = '{{ .Site.Params.ShortTitle }}_';
 const VERSION = 'version_02';
 
-// The files to make available for offline use. make sure to add others to this list
-const URLS = [
-  `/`,
-  `/en/`,
-  `/de/`,
-]
+// Define the base URLs for different languages
+const BASE_URLS = [
+  '/',
+  '/en/',
+  '/de/',
+];
 
-const CACHE_NAME = APP_PREFIX + VERSION
+// Array to store generated URLs
+let URLS = [
+  {{ range (readDir "public") }}
+    '{{ .Name }}',
+    {{ if .IsDir }}
+      {{ $dir := .Name }}
+      {{ $baseDir := print "public/" $dir }}
+      {{ range $file := readDir $baseDir }}
+        {{ if $file.IsDir }}
+          {{ range (readDir (print $baseDir "/" $file.Name)) }}
+            '{{ print $dir "/" $file.Name "/" .Name }}',
+          {{ end }}
+        {{ else }}
+          '{{ print $dir "/" $file.Name }}',
+        {{ end }}
+      {{ end }}
+    {{ end }}
+  {{ end }}
+];
+
+URLS.forEach(element => {
+  console.log(element)
+});
+
+const CACHE_NAME = APP_PREFIX + VERSION;
+
 self.addEventListener('fetch', function (e) {
   console.log('Fetch request : ' + e.request.url);
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) {
         console.log('Responding with cache : ' + e.request.url);
-        return request
+        return request;
       } else {
         console.log('File is not cached, fetching : ' + e.request.url);
-        return fetch(e.request)
+        return fetch(e.request);
       }
     })
-  )
-})
+  );
+});
 
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       console.log('Installing cache : ' + CACHE_NAME);
-      return cache.addAll(URLS)
+      return cache.addAll(URLS);
     })
-  )
-})
+  );
+});
 
 self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
       var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
+        return key.indexOf(APP_PREFIX);
+      });
       cacheWhitelist.push(CACHE_NAME);
       return Promise.all(keyList.map(function (key, i) {
         if (cacheWhitelist.indexOf(key) === -1) {
           console.log('Deleting cache : ' + keyList[i]);
-          return caches.delete(keyList[i])
+          return caches.delete(keyList[i]);
         }
-      }))
+      }));
     })
-  )
-})
+  );
+});
