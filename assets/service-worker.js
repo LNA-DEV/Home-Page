7326c8abeaceb2e2fd6d5ae063a01cb91b921ba8
Lukas Nagel <lukas@lna-dev.net>
2024-03-15 19:22:39 +0100

    ✨ Working offline mode

--- assets/service-worker.js
+++ assets/service-worker.js
@@ -3,12 +3,13 @@ const VERSION = 'version_{{ now.Unix }}'; // Use Hugo's date format to get the b
 
 // Array to store generated URLs
 let URLS = [
+  '/',
   {{- $publicDir := readDir "public" -}}
   {{- range $i, $dir := $publicDir -}}
     {{- if $dir.IsDir -}}
       {{- $files := readDir (printf "public/%s" $dir.Name) -}}
       {{- range $j, $file := $files -}}
-        '{{- printf "%s/%s" $dir.Name $file.Name -}}',
+        '{{- printf "%s/%s/" $dir.Name $file.Name -}}',
       {{- end -}}
       {{- template "listSubDirectories" (slice $dir.Name) -}}
     {{- else -}}
@@ -19,13 +20,13 @@ let URLS = [
 
 {{- define "listSubDirectories" -}}
   {{- $dirName := index . 0 -}}
-  '{{- print $dirName -}}',
+  '{{- printf "%s/" $dirName -}}',
   {{- $subDirs := readDir (printf "public/%s" $dirName) -}}
   {{- range $subDir := $subDirs -}}
     {{- if $subDir.IsDir -}}
       {{- $subDirName := $subDir.Name -}}
       {{- $subFiles := readDir (printf "public/%s/%s" $dirName $subDirName) -}}
-      '{{- printf "%s/%s" $dirName $subDirName -}}',
+      '{{- printf "%s/%s/" $dirName $subDirName -}}',
       {{- range $k, $subFile := $subFiles -}}
         {{- if not $subFile.IsDir -}}
           '{{- printf "%s/%s/%s" $dirName $subDirName $subFile.Name -}}',
@@ -43,25 +44,31 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 self.addEventListener('fetch', function (e) {
   console.log('Fetch request : ' + e.request.url);
   e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
+    (async () => {
+      let response = await caches.match(e.request);
+
+      if (response != null) {
         console.log('Responding with cache : ' + e.request.url);
-        return request;
+        return response;
       } else {
         console.log('File is not cached, fetching : ' + e.request.url);
-        return fetch(e.request);
+        return await fetch(e.request);
       }
-    })
+    })()
   );
 });
 
+
 self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
       console.log('Installing cache : ' + CACHE_NAME);
 
-      return cache.addAll(URLS);
-    })
+      URLS.forEach(async element =>{
+        await cache.add(element);
+      });
+    })(),
   );
 });
 
